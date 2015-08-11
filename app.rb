76f5df3aa39a8bcc40f4a4bd22dc7265cd836145
Ronando Long & Lisa Folliard <student@epicodus.com>
2015-08-11 12:33:19 -0700

    Complete layout.erb, results page pass, all specs pass

--- app.rb
+++ app.rb
@@ -4,5 +4,13 @@ require('./lib/task')
 also_reload('lib/**/*.rb')
 
 get('/') do
+  @tasks = Task.all()
   erb(:index)
+end
+
+post('/tasks') do
+  description = params.fetch("description")
+  task = Task.new(description)
+  task.save()
+  erb(:tasks)  
 end
--- spec/task_integration_spec.rb
+++ spec/task_integration_spec.rb
@@ -8,6 +8,6 @@ describe('task description path', {:type => :feature}) do
     visit('/')
     fill_in("description", :with => 'buy her purse')
     click_button("Enter today's task!")
-    expect(page).to have_content('buy her purse')
+    expect(page).to have_content('successfully submitted!')
   end
 end
